ae9bbb31ade88e8e6dbfa54b1935f77e62ddd8c0
mswvgc <98501566+mswvgc@users.noreply.github.com>
2023-04-12 23:36:56 -0500

    Update program3.rkt

--- program3.rkt
+++ program3.rkt
@@ -1,20 +1,30 @@
 #lang racket
+(require data/maybe)
+(require data/monad)
+;;;;;;;;Data Structures;;;;;;;;;;;;
+;;;;;;;;Stack Operatsions;;;;;;;;;;
 
+(define (maybe-fail err)
+  (list 'error err))
 
+;; Create an empty stack
 (define empty-stack '())
 
+;; Push item onto stack
 (define (push item stack)
   (cons item stack))
 
+;; Pop item off stack
 (define (pop stack)
   (if (null? stack)
-      (values '() #f) ; Return an empty stack and a #f value to indicate that the stack was empty
-      (values (cdr stack) (car stack)))) ; Return the updated stack and the popped item
+      (maybe-fail "Error: Stack is empty")
+      (just (values (cdr stack) (car stack)))))
 
+;; Peek at the top item on the stack
 (define (peek stack)
   (if (null? stack)
-      #f ; Return #f if the stack is empty
-      (car stack))) ; Return the top item on the stack
+      (maybe-fail "Error: Stack is empty")
+      (just (car stack))))
 
 (define (stack-empty? stack)
   (null? stack))
@@ -30,84 +40,80 @@
 
 (define (div x y)
   (if (= y 0)
-      #f ; Return #f to indicate division by zero error
-      (/ x y)))
+      (maybe-fail "Division by zero")
+      (just (/ x y))))
 
 (define (clear-stack stack)
-  empty-stack)
+  (just empty-stack))
 
 (define (show-stack stack)
   (display stack)
   (newline)
-  stack)
+  (just stack))
 
+;; Display the top item on the stack
 (define (top-stack stack)
   (if (stack-empty? stack)
-      (begin
-        (display "Error: Stack is empty")
-        (newline))
+      (maybe-fail "Error: Stack is empty")
       (begin
         (display (peek stack))
-        (newline)))
-  stack)
+        (newline)
+        (just stack))))
 
 (define (size-stack stack)
   (display (length stack))
   (newline)
-  stack)
+  (just stack))
 
 (define (duplicate-top stack)
   (if (stack-empty? stack)
-      (begin
-        (display "Error: Stack is empty")
-        (newline)
-        stack)
-      (push (peek stack) stack)))
+      (maybe-fail "Error: Stack is empty")
+      (push (from-just (peek stack)) stack)))
 
 (define (apply-operation op stack)
-  (maybe
-   (lambda (err)
-     (begin
-       (display "Error: ")
-       (display err)
-       (newline)
-       stack))
-   (case op
-     ((ADD)
-      (if (< (length stack) 2)
-          (maybe-fail "Too few items on stack")
-          (let* ((stack-after-pop1 item1) (pop stack))
-                 (stack-after-pop2 item2) (pop stack-after-pop1))
-            (push (add item2 item1) stack-after-pop2))))
-     ((SUB)
-      (if (< (length stack) 2)
-          (maybe-fail "Too few items on stack")
-          (let* ((stack-after-pop1 item1) (pop stack))
-                 (stack-after-pop2 item2) (pop stack-after-pop1))
-            (push (sub item2 item1) stack-after-pop2))))
-     ((MUL)
-      (if (< (length stack) 2)
-          (maybe-fail "Too few items on stack")
-          (let* ((stack-after-pop1 item1) (pop stack))
-                 (stack-after-pop2 item2) (pop stack-after-pop1))
-            (push (mul item2 item1) stack-after-pop2))))
-     ((DIV)
-      (if (< (length stack) 2)
-          (maybe-fail "Too few items on stack")
-          (let* ((stack-after-pop1 item1) (pop stack))
-                 (stack-after-pop2 item2) (pop stack-after-pop1))
-            (if (zero? item1)
-                (maybe-fail "Division by zero")
-                (push (div item2 item1) stack-after-pop2))))))
-     ((CLR) (clear-stack stack))
-     ((SHOW) (show-stack stack))
-     ((TOP) (top-stack stack))
-     ((SIZ) (size-stack stack))
-     ((DUP) (duplicate-top stack))
-     ((END) (clear-stack stack))
-     (else (maybe-fail (format "Unknown operation: ~a" op))))))
+  (case op
+    ((ADD SUB MUL DIV)
+     (if (< (length stack) 2)
+         (maybe-fail "Too few items on stack")
+         (let* ([stack-after-pop1 (from-just (pop stack))]
+                [item1 (car stack-after-pop1)]
+                [stack-after-pop2 (from-just (pop stack-after-pop1))]
+                [item2 (car stack-after-pop2)])
+           (case op
+             ((ADD) (just (add item2 item1) stack-after-pop2))
+             ((SUB) (just (sub item2 item1) stack-after-pop2))
+             ((MUL) (just (mul item2 item1) stack-after-pop2))
+             ((DIV) (div item2 item1))))))  
+    ((CLR) (just empty-stack))
+    ((SHOW) (just (show-stack stack)))
+    ((TOP) (just (top-stack stack)))
+    ((SIZ) (just (size-stack stack)))
+    ((DUP) (just (duplicate-top stack)))
+    ((END) (just empty-stack))
+    (else (maybe-fail (format "Unknown operation: ~a" op)))))
+
+(define (process-command input stack)
+  (apply-operation (string->symbol (string-upcase input)) stack))
 
 
-     (define (get-command-from-user)
-  (let ((input (read-line)))
-    (string->symbol (string-upcase input))))
+(define (main-loop stack)
+  (display "> ")
+  (let* ([input (read-line)]
+         [command (string-upcase input)]
+         [result (process-command command stack)])
+    (cond
+      [(string=? command "END") (void)]
+      [(maybe? result)
+       (begin
+         (display "Error: ")
+         (display (from-just result))
+         (display ", Stack: ")
+         (display stack)
+         (newline))]
+      [else
+       (main-loop (from-just result))])))
+
+(define (run-program)
+  (main-loop empty-stack))
+
+(run-program)
